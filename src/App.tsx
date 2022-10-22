5efcec7f56ce3806d6a46594447cbac885104708
alan910127 <alan469357@gmail.com>
2022-10-22 15:19:40 +0800

    feat: Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,38 @@
-import { useEffect, useRef } from "react";
-import io from "socket.io-client";
-
-const ws = io("ws://localhost", {
-  path: "/ws/socket.io",
-  transports: ["websocket", "polling"],
-});
-
-const servers: RTCConfiguration = {
-  iceServers: [
-    {
-      urls: [
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302",
-      ],
-    },
-  ],
-};
-
-const pc = new RTCPeerConnection(servers);
+import { Route, Routes } from "react-router-dom";
+import { useOnSocketConnect } from "./hooks/useOnSocketConnect";
+import MainLayout from "./layout/MainLayout";
+import { FeaturePage } from "./pages/FeaturePage";
+import { HomePage } from "./pages/HomePage";
+import { MonitorPage } from "./pages/MonitorPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { StreamPage } from "./pages/StreamPage";
+import { useStore } from "./store/useStore";
 
 function App() {
-  const serverId = useRef("");
+  const setReceiver = useStore((state) => state.setReceiver);
+  const wsStream = useStore((store) => store.socketStream);
+  const wsMonitor = useStore((store) => store.socketMonitor);
 
-  useEffect(() => {
-    ws.on("receiver", ({ server }: { server: string }) => {
-      serverId.current = server;
-    });
-  }, []);
+  useOnSocketConnect(wsStream, ({ server }) => {
+    console.log({ server });
+    setReceiver(server);
+  });
 
-  const handleSendOfferClicked = async () => {
-    // pc.onicecandidate = (event) => {
-    //   if (event.candidate) {
-    //     console.log("ice candidate:", event.candidate);
-    //   }
-    // };
-
-    const localStream = await navigator.mediaDevices.getUserMedia({
-      video: { width: 700, height: 700 },
-    });
-
-    localStream.getTracks().forEach((track) => {
-      pc.addTrack(track, localStream);
-    });
-
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
-
-    // console.log({ offer });
-
-    ws.emit("offer", {
-      target: serverId.current,
-      offer: { sdp: offer.sdp, type: offer.type },
-    });
-
-    ws.on("answer", ({ answer }: { answer: RTCSessionDescriptionInit }) => {
-      pc.setRemoteDescription(answer);
-      console.log(answer);
-    });
-  };
+  useOnSocketConnect(wsMonitor, ({ server }) => {
+    console.log({ server });
+    setReceiver(server);
+  });
 
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen">
-      <button onClick={handleSendOfferClicked}>Send Offer</button>
-    </div>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/feature" element={<FeaturePage />} />
+        <Route path="/stream" element={<StreamPage />} />
+        <Route path="/monitor" element={<MonitorPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </MainLayout>
   );
 }
 
