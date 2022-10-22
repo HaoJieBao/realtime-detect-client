0cd0aa9ddfc04aa1fb1cf9238fd0db2fbccb3b11
alan910127 <alan469357@gmail.com>
2022-10-22 15:20:54 +0800

    feat: Common createOffer function

---
+++ src/utils/createOffer.ts
@@ -0,0 +1,24 @@
+import { Socket } from "socket.io-client";
+
+export const createOffer = async ({
+  pc,
+  ws,
+  receiverId,
+  category,
+}: {
+  pc: RTCPeerConnection;
+  ws: Socket;
+  receiverId: string;
+  category: string;
+}) => {
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+
+  console.log(offer);
+
+  ws.emit("offer", {
+    target: receiverId,
+    offer: { sdp: offer.sdp, type: offer.type },
+    category,
+  });
+};
